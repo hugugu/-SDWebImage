195d0a49cadb71b92b55e82e9ae09f221f8d2186
hugugu <jkthzf_1116@163.com>
2017-05-04 19:09:19 +0800

    项目创建

---
+++ SDImage/SDImage/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SDImage
+//
+//  Created by 胡古古 on 2017/5/4.
+//  Copyright © 2017年 hugugu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ SDImage/SDImage/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  SDImage
+//
+//  Created by 胡古古 on 2017/5/4.
+//  Copyright © 2017年 hugugu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
